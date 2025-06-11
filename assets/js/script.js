61d83dbef7eb00fb9065c6bf49ad045433d3c22c
Abdul Raheem <92326553+RaheemBhatti@users.noreply.github.com>
2025-06-12 01:32:22 +0500

    AOS Animation Added.
    AOS Animation added on home page.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,26 @@
+{
+  AOS.init({
+    duration: 500,
+    
+  });
+}
+
 function handleToggle() {
-    const sideBar = document.querySelector(".head_nav_clmn");
-    const toggler = document.querySelector(".nav-toggle");
-    const overlay = document.querySelector(".nv-ovrlay");
-    const closer = document.querySelector(".nv-close");
-  
-    toggler.addEventListener("click", function () {
-      toggler.classList.toggle("active");
-      sideBar.classList.toggle("active");
+  const sideBar = document.querySelector(".head_nav_clmn");
+  const toggler = document.querySelector(".nav-toggle");
+  const overlay = document.querySelector(".nv-ovrlay");
+  const closer = document.querySelector(".nv-close");
+
+  toggler.addEventListener("click", function () {
+    toggler.classList.toggle("active");
+    sideBar.classList.toggle("active");
+  });
+
+  [overlay, closer].forEach(function (item) {
+    item.addEventListener("click", function () {
+      toggler.classList.remove("active");
+      sideBar.classList.remove("active");
     });
-  
-    [overlay, closer].forEach(function (item) {
-      item.addEventListener("click", function () {
-        toggler.classList.remove("active");
-        sideBar.classList.remove("active");
-      });
-    });
-  }
-  handleToggle();
+  });
+}
+handleToggle();
